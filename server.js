f54e923857067a298e32ddec11791f679c45d250
CatalinIuga <cataliniuga34@gmail.com>
2022-06-28 20:30:04 +0300

    fixed server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const app = express();``
+const app = express(); ``
 const http = require('http');
-const { dirname } = require('path');
+// const { dirname } = require('path');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () => {
